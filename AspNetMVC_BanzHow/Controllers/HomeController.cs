3d6fac1acb087b592a3dcc16e0f4042af02ce458
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-31 11:07:40 +0300

    Master-Tema uygulamaya giydirildi.

---
+++ AspNetMVC_BanzHow/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_BanzHow.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}

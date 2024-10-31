4d0e53fd35b1a50d3f77e2887628455974a38249
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-31 14:30:25 +0300

    ModelList-HomeIndex'ten view'e model gonderildi ve sayfada bootstrap'a uygun olarak gosterildi

--- AspNetMVC_BanzHow/Controllers/HomeController.cs
+++ AspNetMVC_BanzHow/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetMVC_BanzHow.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,22 @@ namespace AspNetMVC_BanzHow.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            string[] kurslar = { "MVC kursu", "web forms", "c# kursu", "MVC kursu", "web forms", "c# kursu" };
-            return View(kurslar);
+
+            List<Urun> urunler = new List<Urun>()
+            {
+                new Urun(){UrunId=1,UrunAdi="Samsung S6",Aciklama="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",Fiyat=1000,Satistami=true},
+                new Urun(){UrunId=2,UrunAdi="Samsung S7",Aciklama="İdare eder",Fiyat=1000,Satistami=false},
+                new Urun(){UrunId=3,UrunAdi="Samsung S8",Aciklama="İdare eder",Fiyat=2000,Satistami=true},
+                new Urun(){UrunId=4,UrunAdi="Iphone 6S",Aciklama="İdare eder",Fiyat=3000,Satistami=false},
+                new Urun(){UrunId=5,UrunAdi="Iphone 7S",Aciklama="İdare eder",Fiyat=3000,Satistami=true},
+                new Urun(){UrunId=6,UrunAdi="Iphone 7S",Aciklama="İdare eder",Fiyat=3000,Satistami=false},
+                new Urun(){UrunId=7,UrunAdi="Iphone 7S",Aciklama="İdare eder",Fiyat=3000,Satistami=true},
+                new Urun(){UrunId=8,UrunAdi="Iphone 7S",Aciklama="İdare eder",Fiyat=3000,Satistami=true},
+                new Urun(){UrunId=9,UrunAdi="Iphone 7S",Aciklama="İdare eder",Fiyat=3000,Satistami=false}
+            };
+
+
+            return View(urunler);
         }
 
         public ActionResult Services()
---
+++ AspNetMVC_BanzHow/Models/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC_BanzHow.Models
+{
+    public class Urun
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; }
+        public string Aciklama { get; set; }
+        public double Fiyat { get; set; }
+        public bool Satistami { get; set; }
+    }
+}
